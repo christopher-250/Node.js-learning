5240d70c1f6a4509ffbd6b5757ab88419daa4261
Christophe Shumbusho <christopheshumbusho69@gmail.com>
2025-06-17 19:46:24 +0200

    commonjs and es module, used for exporting and importing files

---
+++ index.js
@@ -0,0 +1,10 @@
+ import { getPost } from "./postControl.js";
+ console.log(getPost());
+ 
+ 
+ 
+ 
+ /*const {generateRandomNumber,celciusToFahrenheit} = require('./utlis');
+
+ console.log(`Random nunmber: ${generateRandomNumber()}`);
+ console.log(`Celcius to fahrenheit : ${celciusToFahrenheit(0)}`);*/
