36b13983810dae670681cc9d54f41f017fc74cf6
Christophe Shumbusho <christopheshumbusho69@gmail.com>
2025-06-18 14:40:05 +0200

    Request object and env file related codes

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
-import http from 'http'
+import http from 'http';
 
-const PORT = 8000;
+const PORT = process.env.PORT;
 const server = http.createServer((req,res) =>{
-     res.write('ChrisNode');
-     res.end()
+    console.log(req.url);
+    console.log(req.method);
+    /*res.setHeader('Content-Type', 'text/html' );
+    res.statusCode = 404;
+     res.end('<h1>Christophe</h1>');*/
+     res.writeHead(200,{'Content-Type':'text/html'})
+     res.end('<h1>Chris</h1>')
 
 });
 server.listen(PORT , () => {
